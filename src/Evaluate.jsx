c7ce88b2af73efd9698380204dd600492322ccbf
Athil9609 <adhiladhi516@gmail.com>
2024-09-09 22:50:03 +0530

    Add files via upload

--- src/Evaluate.jsx
+++ src/Evaluate.jsx
@@ -20,11 +20,11 @@ function Evaluate() {
     const cal=useSelector((state)=>state.EvalReducer.calculate)
   return (
     <>
-<div className='d-flex justify-content-center align-items-center' style={{height:"100vh"}}>
-    <div className='w-75 shadow p-5'>
+<div className='d-flex justify-content-center align-items-center' style={{height:"90vh"}} >
+    <div className='w-75 shadow p-5 p-sm-2'>
         <h1 className='text-center'>Calculate</h1>
         <input type="text" id="calc" placeholder='Enter an Expression' onChange={(e)=>{setCalcuation(e.target.value)}} className="form-control mt-5" />
-        <h3 className='mt-3'>Result:{cal}</h3>
+        <h3 className='mt-3 text-center'>Result:{cal}</h3>
         <div className='d-flex justify-content-center gap-3 mt-5'>
             <button onClick={()=>{dispatch(Eval(calculation))}} className='btn btn-success'>Calculate</button>
             <button onClick={()=>{dispatch(Reset());resetInp()}} className='btn btn-danger'>Reset</button>
